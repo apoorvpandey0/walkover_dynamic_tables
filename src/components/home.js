4bcd7a700c53c191a3fad3dfdd2042a2d275c74e
krishnmohan19 <krishnmoahn2819@2819@gmail.com>
2022-06-14 17:56:39 +0530

    homejs changed

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import Logout from './logout';
+import { DataTable } from '../table.jsx'
+
+import AddTable from "../modal.jsx";
+
 function Home() {
     const { user } = useAuth0();
+    const [tables, setTables] = React.useState([]);
+    function handleAddNewTable(name, fields) {
+        setTables([...tables, { id: tables.length, name: name, fields: fields, rows: [] }]);
+    }
     return (
         <div className=''>
             <nav class="bg-purple-800 text-white justify-between flex">
@@ -11,10 +19,13 @@ function Home() {
                     <Logout />
                 </ul>
             </nav>
+            <div className='grid place-items-center h-20'>
+                 <AddTable addNewTable={handleAddNewTable} />
+            </div>
             
+            {tables.map(table => <DataTable table={table} />)}
             {console.log(user)}
         </div>
-
     )
 }
 
